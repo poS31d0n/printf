df18bfa27e96fee1956b06fead123db81e7f7e76
pos31d0n <pos31d0n@MacBook-Pro-pos31d0n.local>
2021-11-19 01:29:57 +0300

    fdgh

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,6 +6,18 @@
 int	ft_printf(const char *apFormat, ...)
 {
 	va_list	form;
+	int		i;
 
+	i = 0;
 	ft_format_check(apFormat);
+	while (apFormat[i])
+	{
+		if (apFormat[i++] == '%')
+		{
+			ft_test_1(apFormat, i);
+			ft_test_2(apFormat, i);
+			ft_test_3(apFormat, i);
+		}
+		i++;
+	}
 }
